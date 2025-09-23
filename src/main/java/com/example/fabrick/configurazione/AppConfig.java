bfd6f9df6fc02f064a44c9bde9b93e2d62fd017b
Hamza Habibi <hamzahabibi123@gmail.com>
2025-09-23 14:30:19 +0200

    modifca AppConfig

--- src/main/java/com/example/fabrick/configurazione/AppConfig.java
+++ src/main/java/com/example/fabrick/configurazione/AppConfig.java
@@ -2,12 +2,8 @@ package com.example.fabrick.configurazione;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class AppConfig {
@@ -15,12 +11,6 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
-
-        // ensure Jackson converter is registered
-        List<HttpMessageConverter<?>> converters = new ArrayList<>();
-        converters.add(new MappingJackson2HttpMessageConverter());
-        restTemplate.setMessageConverters(converters);
-
         return restTemplate;
     }
 }
